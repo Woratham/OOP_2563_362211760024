317926bd6109b28e0e7c65787d1594c8b305e63d
Windows 10 <animtuk@gmail.com>
2021-01-05 12:00:38 +0700

    not finish

---
+++ src/LAB3/ex_if_else_if.java
@@ -0,0 +1,21 @@
+package LAB3;
+
+public class ex_if_else_if {
+    public static void main(String[] args) {
+        //ผู้้ใช่ใส่คำคะแนนเป็นจำนวนเต็ม แล้วแสดงผลลัพท์เป็นเกรด
+        Scanner sc =new Scanner (System.in);
+
+        System.out.print("Please,enter your score: ");
+        int score = sc.nextInt();//inp as integer
+        //int score2 = integer.parseInt(sc.nextLine());// input as str
+        //0-49   ==F
+        //50-59  ==D
+        //60-69  ==C
+        //70-79  ==B
+        //80-100 ==A
+        if(scoro <=49) {
+
+        }
+    }
+
+}
---
+++ src/LAB3/ex_switch.java
@@ -0,0 +1,16 @@
+package LAB3;
+
+public class ex_switch {
+
+    public static void main(String[] args) {
+
+        Scanner sc =new Scanner(System.in);
+
+        System.out.print.In("ชอบผลไม้อะไรมากที่สุด ?");
+        System.out.print.In("2.ส้ม");
+        System.out.print.In("3.ทุเรียน");
+        System.out.print.In("4.มะม่วง");
+        System.out.print.In("เลือก (1-4)?");
+        int select = sc.nextInt();
+    }
+}
